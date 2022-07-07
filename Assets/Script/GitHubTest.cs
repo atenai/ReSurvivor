b51c761bead43ae1fb8df1a73247a4a8e5c45bf2
atenai <alfamod47@yahoo.co.jp>
2022-07-07 17:23:38 +0900

    テスト

---
+++ Assets/Script/GitHubTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHubTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //2022/07/07
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
