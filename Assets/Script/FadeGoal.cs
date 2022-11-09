abf5aaac12500c65d5c1ce8768ae5aafa1ce9fbf
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2022-11-09 19:51:53 +0900

    エディター分けようにスクリプトを修正

--- Assets/Script/FadeGoal.cs
+++ Assets/Script/FadeGoal.cs
@@ -3,42 +3,35 @@ using UnityEngine.UI;
 
 public class FadeGoal : MonoBehaviour
 {
-    private float _alfa;
-    //private float _speed = 0.007f;
-    private float _red, _green, _blue;
+    private float alfa = 0.0f ;
 
-    private bool _bFade;
 
-    // Start is called before the first frame update
+    private bool isFade = false;
+
     private void Start()
     {
-        _alfa = 0.0f;
-        _bFade = false;
-        _red = GetComponent<Image>().color.r;
-        _green = GetComponent<Image>().color.g;
-        _blue = GetComponent<Image>().color.b;
+
     }
 
-    // Update is called once per frame
     private void Update()
     {
         if (Goal.b_GOAL)
         {
-            _bFade = true;
+            isFade = true;
         }
 
-        if (_bFade)
+        if (isFade)
         {
-            GetComponent<Image>().color = new Color(_red, _green, _blue, _alfa);
-            _alfa += Time.deltaTime;
+            GetComponent<Image>().color = new Color(GetComponent<Image>().color.r, GetComponent<Image>().color.g, GetComponent<Image>().color.b, alfa);
+            alfa += Time.deltaTime;
 
         }
 
-        if (_alfa >= 1)
+        if (alfa >= 1)
         {
             //ステージ１シーンへ
             StageSceneController.GameClear();
-            _bFade = false;
+            isFade = false;
         }
     }
 }
--- Assets/Script/ScreenSetting.cs
+++ Assets/Script/ScreenSetting.cs
@@ -5,18 +5,26 @@ public class ScreenSetting : MonoBehaviour
 
     private void Awake()
     {
+
+#if UNITY_ANDROID//端末がAndroidだった場合の処理
+
+#endif //終了
+
+#if UNITY_IOS//端末がiPhoneだった場合の処理
+        
+#endif //終了
+
+#if UNITY_STANDALONE//端末がPCだった場合の処理
+
         Screen.SetResolution(1920, 1080, true, 60);
+
+#endif //終了
+
+#if UNITY_EDITOR//Unityエディター上での処理
+
+        Screen.SetResolution(1920, 1080, true, 60);
+
+#endif //終了
     }
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        
-    }
 }
