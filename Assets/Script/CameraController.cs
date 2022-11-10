9010cfe1023c92ec69e315992a15332487e97c8f
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2022-11-10 16:56:17 +0900

    カメラに親オブジェクトを追加

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -3,30 +3,34 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    private GameObject _player;
-    private GameObject _goalPlane;
-    private float _pullBackSpeed;
+    GameObject player;
+    GameObject goalPlane;
+    float pullBackSpeed;
 
-    private void Start()
+    void Start()
     {
-        _player = GameObject.FindGameObjectWithTag("Player");
-        _pullBackSpeed = 5.0f;
+        player = GameObject.FindGameObjectWithTag("Player");
+        pullBackSpeed = 5.0f;
     }
 
-    private void Update()
+    void Update()
     {
         if (!Goal.isGOAL)
         {
-            var tmp = _player.transform.position;
-            var x = tmp.x + 5.0f;
-            transform.position = new Vector3(x, 3.0f, -10.0f);
+            var playerPos = player.transform.position;
+            var cameraParentPosX = playerPos.x + 5.0f;
+            this.transform.position = new Vector3(cameraParentPosX, 3.0f, -10.0f);
         }
         else
         {
-            var position = transform.position;
-            transform.position = new Vector3(position.x, position.y, position.z - _pullBackSpeed * Time.deltaTime);
+            GoalCameraMove();
         }
+    }
 
+    void GoalCameraMove()
+    {
+        var position = this.transform.position;
+        this.transform.position = new Vector3(position.x, position.y, position.z - pullBackSpeed * Time.deltaTime);
     }
 
     public void Shake(float duration, float magnitude)
