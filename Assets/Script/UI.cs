40b9fb52caa5f3f1be559f64e0b961f9ed8d8297
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2022-11-10 13:43:30 +0900

    カウントダウンシステムの作成

--- Assets/Script/UI.cs
+++ Assets/Script/UI.cs
@@ -1,31 +1,58 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UI : MonoBehaviour
 {
+    //リロード画像
     Color reloadColor = new Color(255.0f, 255.0f, 255.0f, 0.0f);
     [SerializeField] GameObject imageReload;
     [SerializeField] Player3D player;
     float RotateSpeed = -500.0f;
 
+    //弾数
     [SerializeField] Text textMagazine;
+
+    //カウントダウン
+    [SerializeField] TextMeshProUGUI timerTMP;
+    [SerializeField] int minute = 10;
+    [SerializeField] float seconds = 0.0f;
+    /// <summary>
+    /// totalTImeは秒で集計されている
+    /// </summary>
+    float totalTime = 0.0f;
 
     void Start()
     {
-        imageReload.GetComponent<Image>().color = reloadColor;
-        textMagazine.text = player.magazine.ToString();
+        StartImageReload();
+
+        StartTextMagazine();
     }
 
     void LateUpdate()
     {
-        imageReload.GetComponent<RectTransform>().transform.Rotate(0.0f, 0.0f, RotateSpeed* Time.deltaTime);
+        LateUpdateImageReload();
+
+        LateUpdateTextMagazine();
+
+        LateUpdateTimerSystem();
+    }
+
+    void StartImageReload()
+    {
+        imageReload.GetComponent<Image>().color = reloadColor;
+    }
+
+    void LateUpdateImageReload()
+    {
+        imageReload.GetComponent<RectTransform>().transform.Rotate(0.0f, 0.0f, RotateSpeed * Time.deltaTime);
 
         if (player.isReloadTimeActive == true)
         {
             if (reloadColor.a <= 1)
             {
-                reloadColor.a += Time.deltaTime * 2.0f; //アルファ値を徐々に＋する
-                imageReload.GetComponent<Image>().color = reloadColor; //画像の透明度を変える
+                reloadColor.a += Time.deltaTime * 2.0f;
+                imageReload.GetComponent<Image>().color = reloadColor;
             }
         }
 
@@ -33,11 +60,37 @@ public class UI : MonoBehaviour
         {
             if (reloadColor.a >= 0)
             {
-                reloadColor.a -= Time.deltaTime * 2.0f; //アルファ値を徐々に-する
-                imageReload.GetComponent<Image>().color = reloadColor; //画像の透明度を変える
+                reloadColor.a -= Time.deltaTime * 2.0f;
+                imageReload.GetComponent<Image>().color = reloadColor;
             }
         }
+    }
 
+    void StartTextMagazine()
+    {
         textMagazine.text = player.magazine.ToString();
+    }
+
+    void LateUpdateTextMagazine()
+    {
+        textMagazine.text = player.magazine.ToString();
+    }
+
+    void LateUpdateTimerSystem()
+    {
+        totalTime = (minute * 60) + seconds;
+        totalTime = totalTime - Time.deltaTime;
+
+        minute = (int)totalTime / 60;
+        seconds = totalTime - (minute * 60);
+
+        if (minute <= 0 && seconds <= 0.0f)
+        {
+            timerTMP.text = "00" + ":" + "00";
+        }
+        else
+        {
+            timerTMP.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+        }
     }
 }
