703f8feb2702cd49e0404ba3c2f3aef0b0c361d3
atenai <alfamod47@yahoo.co.jp>
2022-11-19 04:48:03 +0900

    スクリプトの整理

--- Assets/Script/Player/Bullet.cs
+++ Assets/Script/Player/Bullet.cs
@@ -2,18 +2,6 @@
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Boss") || collision.gameObject.CompareTag("Tank") || collision.CompareTag("Kabe") || collision.CompareTag("Enemy") || collision.CompareTag("technical"))
@@ -29,12 +17,4 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);//このオブジェクトを削除
         }
     }
-
-    //private void OnCollisionEnter2D(Collision2D hit)
-    //{
-    //    if (hit.gameObject.CompareTag("Tank") || hit.gameObject.CompareTag("Kabe") || hit.gameObject.CompareTag("Enemy"))
-    //    {
-    //        Destroy(this.gameObject);//このオブジェクトを削除
-    //    }
-    //}
 }
