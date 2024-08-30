518600937bb6e1529c6209bed7172bb8836b0ee0
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2024-08-31 06:10:04 +0900

    Unityのバージョンをアップデート

--- Assets/FromStore/Arrow WayPointer/Scripts/Waypoint.cs
+++ Assets/FromStore/Arrow WayPointer/Scripts/Waypoint.cs
@@ -9,25 +9,31 @@ namespace FromStore.Arrow_WayPointer.Scripts
 		[HideInInspector] public WaypointController waypointController;
 		[HideInInspector] public int waypointNumber;
 
-		private void Update(){
-			if (waypointController.player) {
-				if(Vector3.Distance(transform.position, waypointController.player.position) < radius){
-					waypointController.ChangeTarget ();
+		private void Update()
+		{
+			if (waypointController.player)
+			{
+				if (Vector3.Distance(transform.position, waypointController.player.position) < radius)
+				{
+					waypointController.ChangeTarget();
 				}
 			}
 		}
 
-		private void OnTriggerEnter (Collider col) {
-			if(col.gameObject.tag == "Player"){
-				waypointController.WaypointEvent (waypointNumber);
-				waypointController.ChangeTarget ();
+		private void OnTriggerEnter(Collider col)
+		{
+			if (col.gameObject.tag == "Player")
+			{
+				waypointController.WaypointEvent(waypointNumber);
+				waypointController.ChangeTarget();
 			}
 		}
 
-		#if UNITY_EDITOR
-		private void OnDrawGizmosSelected(){
-			if (waypointController != null) waypointController.OnDrawGizmosSelected (radius);
+#if UNITY_EDITOR
+		private void OnDrawGizmosSelected()
+		{
+			if (waypointController != null) waypointController.OnDrawGizmosSelected();
 		}
-		#endif
+#endif
 	}
 }
