ebcbcb20621aa6fcd699bed718e7047620c86062
atenai <alfamod47@yahoo.co.jp>
2025-10-29 00:29:54 +0900

    バージョンを更新とインプットシステムの修正

--- Assets/Script/InputSystem/LeftStickTest.cs
+++ Assets/Script/InputSystem/LeftStickTest.cs
@@ -3,16 +3,16 @@ using UnityEngine.InputSystem;
 
 public class LeftStickTest : MonoBehaviour
 {
-    void Update()
-    {
-        var current = Gamepad.current;
+	void Update()
+	{
+		var current = Gamepad.current;
 
-        if (current == null)
-        {
-            return;
-        }
+		if (current == null)
+		{
+			return;
+		}
 
-        var leftStickValue = current.leftStick.x.ReadValue();
-        Debug.Log("x‚ÌˆÚ“®—Ê : " + leftStickValue);
-    }
+		var leftStickValue = current.leftStick.x.ReadValue();
+		Debug.Log("leftStickValue : " + leftStickValue);
+	}
 }
